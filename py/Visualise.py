1a9d34a5e253f421d0c739dc60f29c6b2056d9be
john681611 <john.harvey@marks-and-spencer.com>
2017-07-10 11:19:11 +0100

    Basic prints

--- py/Visualise.py
+++ py/Visualise.py
@@ -1,10 +1,37 @@
+from random import randint
+
+
 def rngfunc():
-    value = 0
+    value = randint(0, 9)
     return value
 
 
 def iterate(x=0, y=10000):
+    response = {}
     arry = []
-    for i in range(x,y):
+    for i in range(x, y):
         arry.append(rngfunc())
-    return arry
+    response["raw"] = arry
+
+    counts = {}
+    percent = {}
+    for item in arry:
+        counts[item] = arry.count(item)
+        percent[item] = counts[item]/((y-x)/100)
+    response["counts"] = counts
+    response["percents"] = percent
+
+    return response
+
+def visualise():
+    x = 0
+    y = 10000
+    response = iterate(x,y)
+    print("Percentages : " + str(response["percents"]))
+    print("Counts : " + str(response["counts"]))
+    print("Raw : " + str(response["raw"]))
+
+
+if __name__ == "__main__":
+    visualise()
+
