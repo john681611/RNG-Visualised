2b94666e85aa490aa775c0ed7312a50099b0b43f
john681611 <john.harvey@marks-and-spencer.com>
2017-07-11 10:07:44 +0100

    neat little graph

--- py/Visualise.py
+++ py/Visualise.py
@@ -1,27 +1,27 @@
-from random import randint
-
+import random
 
 def rngfunc():
-    value = randint(0, 9)
+    value = int(random.triangular(0, 9, 1))
     return value
 
 
 def iterate(x=0, y=10000):
     response = {}
-    arry = []
+    array = []
     for i in range(x, y):
-        arry.append(rngfunc())
-    response["raw"] = arry
+        array.append(rngfunc())
+    response["raw"] = array
 
     counts = {}
     percent = {}
-    for item in arry:
-        counts[item] = arry.count(item)
-        percent[item] = counts[item]/((y-x)/100)
+    for item in array:
+        counts[item] = array.count(item)
+        percent[item] = str(counts[item]/((y-x)/100)) + "%"
     response["counts"] = counts
     response["percents"] = percent
 
     return response
+
 
 def visualise():
     x = 0
@@ -30,6 +30,12 @@ def visualise():
     print("Percentages : " + str(response["percents"]))
     print("Counts : " + str(response["counts"]))
     print("Raw : " + str(response["raw"]))
+    for index, item in response["percents"].items():
+        temp = int(round(float(item.strip('%'))))
+        stars = ""
+        for i in range(0, temp):
+            stars = stars + "*"
+        print(str(index) + " : " + stars + " : " + item)
 
 
 if __name__ == "__main__":
